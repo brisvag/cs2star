feb40f3e2827f5eceb133a8801c3466face55614
Lorenzo Gaifas <brisvag@gmail.com>
2021-02-08 09:31:15 +0100

    better logs

--- cs2star.py
+++ cs2star.py
@@ -8,7 +8,6 @@ import sys
 from pathlib import Path
 import shutil
 import warnings
-from pprint import pformat
 
 import pandas as pd
 import numpy as np
@@ -43,7 +42,7 @@ def parse():
 
 def main():
     parser, args = parse()
-    log = {}
+    log = ['=' * 80, 'Parameters:']
 
     # interpret args
     # get all the particle files
@@ -61,24 +60,26 @@ def main():
             cs_inputs.append(f)
     if not cs_inputs:
         parser.error('no usable particle positions files were found')
-    log['Particle files'] = cs_inputs
-    log['Passthrough files'] = cs_passthroughs
+    log.append(f'Particle files: {[str(f) for f in cs_inputs]}')
+    log.append(f'Passthrough files: {[str(f) for f in cs_passthroughs]}')
 
     cs_img = path(args.extraction) / 'extract'
     if not cs_img.is_dir():
         parser.error('the micrographs directory does not exist. Did you provide the right job?')
-    log['Micrograph directory'] = cs_img
+    log.append(f'Micrograph directory: {str(cs_img)}')
 
     dest = path(args.destination)
     dest_img = dest / 'images'
-    dest_star = dest / f'particles.star'
+    dest_star = dest / 'particles.star'
     log_file = dest / 'cs2star.log'
     if dest_img.is_dir() or dest_star.is_file():
         if not args.force_overwrite:
             parser.error('some destination files already exists. To overwrite, use -f')
-    log['Destination directory'] = dest
+    log.append(f'Destination directory: {str(dest)}')
+    log.append('=' * 80)
 
-    print('=' * 80, 'Parameters:', pformat(log), '=' * 80, sep='\n')
+    log = '\n'.join(log)
+    print(log)
 
     # stop here on dry run
     if args.dry_run:
@@ -91,17 +92,16 @@ def main():
     cs_passthroughs = [str(f) for f in cs_passthroughs]
     df_all = pd.DataFrame()
     for f in cs_inputs:
-        print(f'converting "{f.name}" to ".star" format...', end='\r')
+        print(f'Converting "{f.name}" to ".star" format...')
         data = np.load(f)
         df = pyem.metadata.parse_cryosparc_2_cs(data, passthroughs=cs_passthroughs,
                                                 minphic=0, boxsize=None, swapxy=False)
         df_all.append(df, ignore_index=True)
-    print()
     if args.classes is not None:
-        print('selecting classes...')
+        print('Celecting classes...')
         df_all = pyem.star.select_classes(df_all, args.classes)
 
-    print('cleaning up data...')
+    print('Cleaning up data...')
     # clean up
     df_all = pyem.star.check_defaults(df_all, inplace=True)
     df_all = pyem.star.remove_deprecated_relion2(df_all, inplace=True)
@@ -109,6 +109,7 @@ def main():
     # fix micrograph paths
     df['rlnMicrographName'] = './images/' + df['rlnMicrographName'].str.split('/').str.get(-1) + 's'
 
+    print('Writing star file...')
     # write to file
     pyem.star.write_star(str(dest_star), df, resort_records=True, optics=True)
 
@@ -116,6 +117,7 @@ def main():
     imgs_orig = cs_img.iterdir()
     imgs_dest = []
     exists = False
+    print(f'{"Copying" if args.copy else "Linking"} images to "{dest_img}"...')
     for mrc in imgs_orig:
         # add s to extension for relion
         mrcs = Path(f'{mrc.relative_to(cs_img)}s')
@@ -133,11 +135,10 @@ def main():
         print('INFO: some files were not symlinked because they already exist')
 
     # write log
-    header = '# this directory was converted from cryosparc with cs2star.py.'
+    header = '# this directory was converted from cryosparc with cs2star.py. Command:'
     command = " ".join(sys.argv)
-    params = pformat(log)
     with open(log_file, 'w+') as f:
-        f.write(f'{header}\nCommand:\n{command}\n{" " * 80}\nParams:\n{params}\n')
+        f.write(f'{header}\n{command}\n{log}\n')
 
     print('Done!')
 
