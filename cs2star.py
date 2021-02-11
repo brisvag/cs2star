ab90d451a8e85b66691051689aafed510c51cc10
Lorenzo Gaifas <brisvag@gmail.com>
2021-02-11 10:16:12 +0100

    updated to click

--- cs2star.py
+++ cs2star.py
@@ -5,12 +5,13 @@ copy and convert a cryosparc dir into a relion dir
 
 import argparse
 import sys
-from pathlib import Path
 import shutil
 import warnings
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
+import click
 
 try:
     import pyem
@@ -22,31 +23,33 @@ except ImportError:
         warnings.filterwarnings('ignore', category=SyntaxWarning)
         import pyem
 
-
-def path(p):
-    return Path(p).expanduser().resolve()
+real_dir = click.Path(exists=True, file_okay=False, resolve_path=True)
 
 
-def parse():
-    parser = argparse.ArgumentParser(prog='cs2star')
-    parser.add_argument('positions', type=Path, help='a cryosparc job containing a "*particles.cs" file. The particles must contain references to images (blobs)')
-    parser.add_argument('extraction', type=Path, help='a cryosparc "extract from micrographs" job directory')
-    parser.add_argument('destination', nargs='?', default=Path.cwd(), type=Path, help='the destination directory')
-    parser.add_argument('--classes', nargs='+', type=int, help='only use particles from these classes')
-    parser.add_argument('-f', '--force-overwrite', action='count', help='overwrite the existing destination directory if present. Passed once, overwrite star file only. Twice, also files/symlinks')
-    parser.add_argument('-d', '--dry-run', action='store_true', help='dry run')
-    parser.add_argument('-c', '--copy', action='store_true', help='copy the images instead of symlinking to them')
-    args = parser.parse_args(sys.argv[1:])
-    return parser, args
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument('positions', type=real_dir)
+@click.argument('extraction', type=real_dir)
+@click.argument('destination', required=False, default='.', type=click.Path(file_okay=False))
+@click.option('--classes', help='only use particles from these classes. Comma-separated list.')
+@click.option('-f', '--overwrite', count=True, help='overwrite the existing destination directory if present.'
+              'Passed once, overwrite star file only. Twice, also files/symlinks')
+@click.option('-d', '--dry-run', is_flag=True)
+@click.option('-c/-s', '--copy/--symlink', help='copy the images or symlink to them')
+def main(positions, extraction, destination, classes, overwrite, dry_run, copy):
+    """
+    Copy and convert a cryosparc dir into a relion-ready dir.
 
+    POSITIONS: a cryosparc job containing a "*particles.cs" file. The particles must contain references to images (blobs).
 
-def main():
-    parser, args = parse()
+    EXTRACTION: a cryosparc "extract from micrographs" job directory.
+
+    DESTINATION: the destination directory.
+    """
     log = ['=' * 80, 'Parameters:']
 
     # interpret args
     # get all the particle files
-    cs_pos = path(args.positions)
+    cs_pos = Path(positions)
     cs_inputs = []
     cs_passthroughs = []
     for f in cs_pos.glob('*particles*.cs'):
@@ -59,16 +62,16 @@ def main():
         else:
             cs_inputs.append(f)
     if not cs_inputs:
-        parser.error('no usable particle positions files were found')
+        click.UsageError('no usable particle positions files were found')
     log.append(f'Particle files: {[str(f) for f in cs_inputs]}')
     log.append(f'Passthrough files: {[str(f) for f in cs_passthroughs]}')
 
-    cs_img = path(args.extraction) / 'extract'
+    cs_img = Path(extraction) / 'extract'
     if not cs_img.is_dir():
-        parser.error('the micrographs directory does not exist. Did you provide the right job?')
+        click.UsageError('the micrographs directory does not exist. Did you provide the right job?')
     log.append(f'Micrograph directory: {str(cs_img)}')
 
-    dest = path(args.destination)
+    dest = Path(destination)
     dest_img = dest / 'images'
     dest_star = dest / 'particles.star'
     log_file = dest / 'cs2star.log'
@@ -76,31 +79,33 @@ def main():
     log.append('=' * 80)
 
     log = '\n'.join(log)
-    print(log)
+    click.secho(log, fg='green')
 
     # stop here on dry run
-    if args.dry_run:
+    if dry_run:
         sys.exit()
 
     # make dest dir
     dest_img.mkdir(parents=True, exist_ok=True)
-    if dest_star.is_file() and args.force_overwrite == 0:
-        parser.error('particle file already exists. To overwrite, use -f')
+    if dest_star.is_file() and overwrite == 0:
+        click.UsageError('particle file already exists. To overwrite, use -f')
 
     # convert positions
     cs_passthroughs = [str(f) for f in cs_passthroughs]
     df_all = pd.DataFrame()
-    for f in cs_inputs:
-        print(f'Converting "{f.name}" to ".star" format...')
-        data = np.load(f)
-        df = pyem.metadata.parse_cryosparc_2_cs(data, passthroughs=cs_passthroughs,
-                                                minphic=0, boxsize=None, swapxy=False)
-        df_all.append(df, ignore_index=True)
-    if args.classes is not None:
-        print('Celecting classes...')
-        df_all = pyem.star.select_classes(df_all, args.classes)
+    with click.progressbar(cs_inputs, label='Converting to ".star" format') as bar:
+        for f in bar:
+            data = np.load(f)
+            df = pyem.metadata.parse_cryosparc_2_cs(data, passthroughs=cs_passthroughs,
+                                                    minphic=0, boxsize=None, swapxy=False)
+            df_all.append(df, ignore_index=True)
 
-    print('Cleaning up data...')
+    if classes is not None:
+        classes = classes.split(',')
+        click.secho('Selecting classes...')
+        df_all = pyem.star.select_classes(df_all, classes)
+
+    click.secho('Cleaning up data...')
     # clean up
     df_all = pyem.star.check_defaults(df_all, inplace=True)
     df_all = pyem.star.remove_deprecated_relion2(df_all, inplace=True)
@@ -108,7 +113,7 @@ def main():
     # fix micrograph paths
     df['rlnMicrographName'] = './images/' + df['rlnMicrographName'].str.split('/').str.get(-1) + 's'
 
-    print('Writing star file...')
+    click.secho('Writing star file...')
     # write to file
     pyem.star.write_star(str(dest_star), df, resort_records=True, optics=True)
 
@@ -116,25 +121,26 @@ def main():
     imgs_orig = cs_img.iterdir()
     imgs_dest = []
     exists = False
-    print(f'{"Copying" if args.copy else "Linking"} images to "{dest_img}"...')
+    click.secho(f'{"Copying" if copy else "Linking"} images to "{dest_img}"...')
     for mrc in imgs_orig:
         # add s to extension for relion
         mrcs = Path(f'{mrc.relative_to(cs_img)}s')
         # save the relative path for later to edit the starfile
         imgs_dest.append(mrcs)
         moved = dest_img / mrcs
-        if moved.is_file() and args.force_overwrite <= 1:
+        if moved.is_file() and overwrite <= 1:
             exists = True
             continue
         else:
-            if args.copy:
+            if copy:
                 shutil.copy(mrc, moved)
             else:
                 if moved.is_symlink():
                     moved.unlink()
                 moved.symlink_to(mrc)
-    if exists and args.force_overwrite <= 1:
-        print('INFO: some files were not symlinked/copied because they already exist. Use -ff to force overwrite')
+    if exists and overwrite <= 1:
+        click.secho('INFO: some files were not symlinked/copied because they already exist.'
+                    'Use -ff to force overwrite', bg='red')
 
     # write log
     header = '# this directory was converted from cryosparc with cs2star.py. Command:'
